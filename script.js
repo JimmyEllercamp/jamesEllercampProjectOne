0e2b46f53248f4242ed7220f6e3074117cd24f36
James <j.ellercamp@gmail.com>
2021-12-14 19:15:55 -0500

    The contact page form now returns a message to the user including their name upon form submission

--- script.js
+++ script.js
@@ -6,18 +6,31 @@ hamburger.addEventListener('click', function(){
     if (hamburger.classList.contains('open')) {
         dropMenu.style.display = "none";
         hamburger.classList.remove('open');
+        hamburger.style.color = 'rgb(246,246,246';
         console.log('Am closedddd');
     } else {
         dropMenu.style.display = "block";
         hamburger.classList.add('open');
+        hamburger.style.color = 'rgb(228, 164, 54)';
         console.log('Am opennnnn');
-    } 
-
-
-    // dropMenu.style.display = 'block';
-    // hamburger.style.color = 'rgb(228, 164, 54)';
-    // // hamburger.style.color.toggle('white');
-    // console.log('dropdown menu style display');
-
+    }
 });
 
+const contactForm = document.querySelector('.contactForm');
+
+const userName = document.querySelector('#userName');
+
+const thankTheUser = document.querySelector('.userThank');
+console.log(thankTheUser);
+contactForm.addEventListener('submit', function(event){
+    event.preventDefault();
+    const userNameInput = userName.value;
+    console.log(userNameInput);
+    userName.value = '';
+    document.querySelector('#userEmail').value = '';
+    document.querySelector('#userMessage').value = '';
+
+    const newParagraph = document.createElement('p');
+    thankTheUser.appendChild(newParagraph);
+    thankTheUser.querySelector('p').textContent = `Thanks, ${userNameInput}!`;
+})
